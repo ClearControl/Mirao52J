b4bcb57c673abf66cc913602055143f28ca6068b
Loic Alain Royer <royerloic@gmail.com>
2018-09-02 22:07:40 -0700

    new build system, upgraded gradle wrapper

--- src/mirao52e/Mirao52Exception.java
+++ src/mirao52e/Mirao52Exception.java
@@ -12,32 +12,32 @@ package mirao52e;
 public class Mirao52Exception extends RuntimeException
 {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Constructs an instance of Mirao52Exception class given an error message.
-	 * 
-	 * @param pExceptionMessage
-	 *          exception message
-	 */
-	public Mirao52Exception(String pExceptionMessage)
-	{
-		super(pExceptionMessage);
-	}
+  /**
+   * Constructs an instance of Mirao52Exception class given an error message.
+   * 
+   * @param pExceptionMessage
+   *          exception message
+   */
+  public Mirao52Exception(String pExceptionMessage)
+  {
+    super(pExceptionMessage);
+  }
 
-	/**
-	 * Constructs an instance of the Mirao52Exception class given an error message
-	 * and a cause.
-	 * 
-	 * @param pExceptionMessage
-	 *          exception message
-	 * @param pException
-	 *          causative exception
-	 */
-	public Mirao52Exception(String pExceptionMessage,
-													Throwable pException)
-	{
-		super(pExceptionMessage, pException);
-	}
+  /**
+   * Constructs an instance of the Mirao52Exception class given an error message
+   * and a cause.
+   * 
+   * @param pExceptionMessage
+   *          exception message
+   * @param pException
+   *          causative exception
+   */
+  public Mirao52Exception(String pExceptionMessage,
+                          Throwable pException)
+  {
+    super(pExceptionMessage, pException);
+  }
 
 }
